d278340298375aa8485dd1bfd88e2c0efada0138
mitriifd <idimka9@gmail.com>
2021-09-02 17:37:42 +0500

    ajax

---
+++ Продвинутый Javascript/ajax/js/script.js
@@ -0,0 +1,40 @@
+
+const inputRub = document.querySelector('#rub')
+const inputUsd = document.querySelector('#usd')
+
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest()
+
+    // request.open(method, url, async, login, pass)
+    request.open('GET', 'js/current.json')
+    request.setRequestHeader('Content-Type', 'application/json; charset=utf8')
+    request.send()
+
+    // request.addEventListener('readystatechange', () => {
+    //     if (request.readyState === 4 && request.status === 200) {
+    //         console.log(request.response);
+    //         const data = JSON.parse(request.response)
+    //         inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2)
+    //     } else {
+    //         inputUsd.value = 'Что-то пошло не так'
+    //     }
+    // })
+
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            console.log(request.response);
+            const data = JSON.parse(request.response)
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2)
+        } else {
+            inputUsd.value = 'Что-то пошло не так'
+        }
+    })
+
+    // status
+    // statusText
+    // response
+    // readyState
+
+
+})
+
